edf79975d24a66b8f2f20ab800922911931c16c2
Garrett <garrettgsb@gmail.com>
2023-03-28 11:33:27 -0700

    After lecture

---
+++ z-beatles-vampire-tree.js
@@ -0,0 +1,7 @@
+const john = new Vampire('John');
+const paul = new Vampire('Paul');
+const george = new Vampire('George');
+new Vampire('Ringo');
+
+john.addOffspring(paul);
+john.addOffspring(george);
---
+++ z-javascript-object-tree.js
@@ -0,0 +1,9 @@
+const data = {
+  users: {
+    abc: { email: 'abc@email.com' },
+    asdf: { email: 'asdf@email.com' },
+  },
+  posts: {
+    post1: { title: 'Post 1', body: 'this is a post' },
+  }
+}
